a441c5b8b6d9f27b25b60a63d62f4b4b0fe2bbdf
ralph squillace <ralph@squillace.com>
2024-03-10 23:14:12 +0100

    now have real variables

--- foo.rs
+++ foo.rs
@@ -3,16 +3,27 @@ use std::io::Write;
 
 fn write(name: std::string::String) {
     let mut f = File::create(name).unwrap();
+    
     writeln!(&mut f, "This is a test").unwrap();
 }
 
 fn main() {
-    
-    println!("Hello rust world!");
+  let an_integer = 1u32;
+  let a_boolean = true;
+  let unit = ();
 
-    write(String::from("test.txt"));
-    println!("Wrote file");
+  // copy `an_integer` into `copied_integer`
+  let copied_integer = an_integer;
 
-    std::fs::copy("test.txt", "test-2.txt").unwrap();
-    println!("Copied file");
+  println!("An integer: {:?}", copied_integer);
+  println!("A boolean: {:?}", a_boolean);
+  println!("Meet the unit value: {:?}", unit);
+
+  println!("Hello rust world!");
+
+  write(String::from("test.txt"));
+  println!("Wrote file");
+
+  std::fs::copy("test.txt", "test-2.txt").unwrap();
+  println!("Copied file");
 }
