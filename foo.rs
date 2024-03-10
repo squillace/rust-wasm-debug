989b6dd89bd95d1129b9662670498a38c1072043
ralph squillace <ralph@squillace.com>
2024-03-10 23:08:31 +0100

    slightly more complex version

--- foo.rs
+++ foo.rs
@@ -1,4 +1,18 @@
+use std::fs::File;
+use std::io::Write;
+
+fn write(name: std::string::String) {
+    let mut f = File::create(name).unwrap();
+    writeln!(&mut f, "This is a test").unwrap();
+}
+
 fn main() {
-  println!("hello from everywhere!");
-  println!("OK, here's another line.");
+    
+    println!("Hello rust world!");
+
+    write(String::from("test.txt"));
+    println!("Wrote file");
+
+    std::fs::copy("test.txt", "test-2.txt").unwrap();
+    println!("Copied file");
 }
